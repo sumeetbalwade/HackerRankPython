cc201f7dccc7fe690c0c7d2ad554fe8e6553f2c2
Sumeet Balwade <balwadesr@gmail.com>
2022-02-14 23:28:40 +0530

    first commit

--- runnerup.py
+++ runnerup.py
@@ -1,4 +1,8 @@
-if __name__ == '__main__':
-    n = int(raw_input())
-    arr = map(int, raw_input().split())
-    print(sorted(list(set(arr)))[-2])
+score_list = []; # do not forget to declare a list
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
+    
+    score_list.append([name, score])
+second_highest = sorted(set([score for name, score in score_list]))[1]
+print('\n'.join(sorted([name for name, score in score_list if score == second_highest])))
